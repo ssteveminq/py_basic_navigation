ff83cda7086544edaf6ee0aa63779c3d89dc14e3
ssteveminq <steveminq@gmail.com>
2020-07-31 15:06:05 -0500

    path obtained

--- gradient_mostrecent.m
+++ gradient_mostrecent.m
@@ -2,18 +2,19 @@
 clc 
 clear all 
 close all
-K=50; %attractive potential 
-E=80; %repulsive potential
+K=35; %attractive potential 
+E=15; %repulsive potential
 % area_width=12; %potential area width (m)
 sx=10; % start pos x
-sy=10; %start pos y
+sy=11; %start pos y
 gx=-1; %goal pos x
-gy=-2; %goal pos y
-ox=[5 6]; %obstacle x pos
-oy= [-3 6]; %obstacle y pos
+gy=-5; %goal pos y
+ox=[5.0 6.05]; %obstacle x pos
+oy= [-3.0 6.05]; %obstacle y pos
 % [x,y]= meshgrid(-12:.5:12); 
-[x,y] = meshgrid(-12:1.0:12,-12:1.0:12);
-r=((x-gx).^2+(y-gx).^2).^.5;%Distance to Goal
+res=0.25;
+[x,y] = meshgrid(-12:res:12,-12:res:12);
+r=((x-gx).^2+(y-gy).^2).^.5;%Distance to Goal
 r1=((x-ox(1)).^2+(y-oy(1)).^2).^.5;%Distance to Obs1
 r2=((x-ox(2)).^2+(y-oy(2)).^2).^.5;%Distance to Obs2
 U=K.*r+E./r1*+E./r2;
@@ -23,7 +24,7 @@ figure(1)
 mesh(x,y,U);
 figure(2)
 contour(x,y,U);
-[Ux,Uy]=gradient(U,1.0);
+[Ux,Uy]=gradient(U,res,res);
 % Ux=diff(U,x,1.0);
 % Uy=diff(U,y,1.0);
 % [J]=-gradient(U);
@@ -48,10 +49,11 @@ contour(x,y,U);
 % grady=grad(:,2);
 % hold on
 [u,v,w] = surfnorm(x,y,U);
-% figure(3)
+figure(3)
 % [x1,y1]=meshgrid(
 % quiver(x,y,0.1*Ux,0.1*Uy)
 % quiver3(x,y,U,u,v,w,0.5)
+quiver(x,y,u,v,res)
 
 % set(h1,'AutoScale','on', 'AutoScaleFactor', 0.5)
 %plot(Ux,Uy);s
@@ -67,8 +69,8 @@ iter=0;
 % maxiter=100;
 errx=1;
 erry=1;
-alpha=.1;
-tol=.75;
+alpha=0.03;
+tol=0.3;
 xi=sx;
 yi=sy;
 gvecx=[];
@@ -82,32 +84,58 @@ contour(x,y,U)
 scatter(sx,sy,'b')
 scatter(gy,gy,'r')
 
-for i=1:25 %used to say -12:.5:12 
-   
-      for j=1:25
-            
+xmin =-12.0;
+ymin =-12.0;
+maxiter=500;
 
-            xnew=xi-alpha*Ux(i,j);
-%             errx = abs((xnew-xi)/xi);
-            ynew=yi-alpha*Uy(i,j);
-%             err = abs((ynew-yi)/yi);
+% for i=1:25 %used to say -12:.5:12 
+   
+%       for j=1:25
+Ux=Ux';
+Uy=Uy';
+  
+while iter<maxiter         
+          ind_x = floor((xi-xmin)/res)
+          ind_y = floor((yi-ymin)/res)
+%           xx = xmin+res*ind_x
+%           yy = ymin+res*ind_y
+          gradx = Ux(ind_x,ind_y)
+          grady = Uy(ind_x,ind_y)
+%           if abs(gradx)<1.0
+%              alpha = 0.2;
+%           else
+%               alpha= 0.05;
+%           end
+%           
+%            if abs(grady)<1.0
+%              alpha_y = 0.5;
+%           else
+%               alpha_y= 0.05;
+%           end
+            xnew=xi-alpha*gradx;
+            ynew=yi-alpha*grady;
+%          err = abs((ynew-yi)/yi);
+      
 
             gvecx=[gvecx;xi];
             gvecy=[gvecy;yi];
 
             iter=iter+1;
-            j=j+1;
+       
             scatter(xi,yi,'g')
-            if xi<=gx+tol && xi>=gx-tol && yi<=gy+tol && yi>=gy-tol
+            if (abs(xnew-gx)<tol) && (abs(ynew-gy)<tol)
                 disp('Goal Reached');
                 disp(xi);
                 disp(yi);
-                
-                
-        
+                break;
+              
             end
             xi=xnew;
-            yi=xnew;
+            yi=ynew;
+            disp("x");
+            disp(xi);
+            disp("y");
+            disp(yi);
           
 %         disp("x is")
 %         disp(x)
@@ -115,8 +143,8 @@ for i=1:25 %used to say -12:.5:12
 %         disp(y)
           
 %         end
-      end
-      i=i+1;
+%       end
+
       
 end
 hold off
@@ -143,22 +171,7 @@ hold off
 % quiver(x,y,Ux,Uy)
 % hold off
 %%
-syms xs ys
-Iter=0;
-Maxiter=25;
-Udx=((50*(xs-1))/(xs^2-2*xs+ys^2+5-4*ys)^.5)-((80*(xs-5))/(xs^2-10*xs+ys^2+34-6*ys)^1.5)-((80*(xs-6))/(xs^2-12*xs+ys^2+72-12*ys)^1.5);
-Udy=((50*(ys-2))/(ys^2-4*ys+xs^2+5-2*xs)^.5)-((80*(ys-3))/(ys^2-6*ys+xs^2+34-10*xs)^1.5)-((80*(ys-6))/(ys^2-12*ys+xs^2+72-12*xs)^1.5);
-while Iter<Maxiter
-    xs=xs-alpha*Udx(xs,yi);
-    ys=ys-alpha*Udx(xs,ys);
-    disp("Iter=");
-    disp(Iter);
-    disp("xi=")
-    disp(xs);
-    disp("yi=");
-    disp(ys);
-end
-%%
+/
 function circle (xc,yc,r)
 ang=0:.01:2*pi;
 xp=r*cos(ang);
